b1d722a6e8c104654629571b6c8cef56d8441e09
Safa <safaanwar2211@gmail.com>
2023-07-14 10:21:52 -0700

    second commits

--- lib/home-screen.dart
+++ lib/home-screen.dart
@@ -12,7 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('safa toptal developer'),
+        title: Text('safa '),
       ),
     );
   }
